1e76d206ad2ed1031f814937875584d84463d09d
themikail <hello@mikailgokce.com>
2023-04-05 11:28:46 +0200

    change

--- js/index.js
+++ js/index.js
@@ -10,34 +10,3 @@ likeButton.addEventListener("click", handleLikeButtonClick);
 
 // Exercise:
 // Use document.createElement() and append another social media post to the body.
-
-const postContainer = document.createElement("article");
-postContainer.classList.add("post");
-
-const postContent = document.createElement("p");
-postContent.classList.add("post__content");
-postContent.textContent =
-  "Lorem ipsum dolor, sit amet consectetur adipisicing elit.";
-
-const postFooter = document.createElement("footer");
-postFooter.classList.add("post__footer");
-
-const postUsername = document.createElement("span");
-postUsername.classList.add("post__username");
-postUsername.textContent = "@username2";
-
-likeButton.classList.add("post__button");
-likeButton.setAttribute("type", "button");
-likeButton.setAttribute("data-js", "like-button");
-likeButton.textContent = "♥ Like";
-
-postFooter.appendChild(postUsername);
-postFooter.appendChild(likeButton);
-
-postContainer.appendChild(postContent);
-postContainer.appendChild(postFooter);
-
-const body = document.querySelector("body");
-body.appendChild(postContainer);
-
-likeButton.addEventListener("click", handleLikeButtonClick);
